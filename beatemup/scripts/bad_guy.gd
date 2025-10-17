9797cc17db8cad61e3aa826bbc86d4b0ae42cb96
Jake Terry <47571311+jaketerry28@users.noreply.github.com>
2025-10-16 20:29:50 -0400

    make beatemup

---
+++ beatemup/scripts/bad_guy.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+var health = 5
+@onready var health_bar = $ProgressBar
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func hit(hit_dir):
+	self.velocity = hit_dir * 100
+	health -= 1
+	health_bar.value = health
+	print(health)
+	
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	self.velocity *= .9
+	move_and_slide()
