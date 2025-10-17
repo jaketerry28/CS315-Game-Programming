fce6ddbdf4e6554b1b2f3521c5b11a1293c64d85
Jake Terry <47571311+jaketerry28@users.noreply.github.com>
2025-10-16 20:35:45 -0400

    moved script

--- beatemup/scripts/bad_guy.gd
+++ beatemup/scripts/bad_guy.gd
@@ -12,8 +12,6 @@ func hit(hit_dir):
 	health -= 1
 	health_bar.value = health
 	print(health)
-	
-	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
