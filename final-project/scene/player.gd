a9b06e5decbe1fc7dc10b4c8ff84aa1e10cb0a22
Jake Terry <47571311+jaketerry28@users.noreply.github.com>
2025-11-20 22:11:37 -0500

    Add legacy character and monster sprite assets
    Added a large collection of legacy character and monster sprite assets, including Aseprite files, PNGs, GIF previews, and import metadata. This update includes assets for various characters (Frog, Mummy, Ogre, Slime, Wizard, Mechanic, Bridge Heroine, Dancing Girl) and monster packs, organized by type and animation. These resources will support game development and animation workflows.

--- final-project/scene/player.gd
+++ final-project/scene/player.gd
@@ -1,25 +1,74 @@
 extends CharacterBody2D
 
+@onready var animation : AnimatedSprite2D = $AnimatedSprite2D
 
-@export var speed : float = 300.0
-@export var jump_velocity : float = -300.0
+@export var gravity : float = 1000
+@export var speed : float = 300
+@export var jump : float = -300
+@export var jump_horizontal : float = 100
 
+enum State {Idle, Run, Jump}
+
+var current_state : State
+var direction
+
+func _ready():
+	current_state = State.Idle
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	player_falling(delta)
+	player_idle(delta)
+	player_run(delta)
+	player_jump(delta)
+	
+	
+	move_and_slide()
+	
+	player_animation()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
+func player_falling(delta: float):
+	if !is_on_floor():
+		velocity.y += gravity * delta
+
+func player_idle(delta: float):
+	if is_on_floor():
+		current_state = State.Idle
+		
+func player_run(delta: float):
+	direction = input_movement()
+	
 	if direction:
 		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, 300)
 
-	move_and_slide()
+	if direction != 0 and is_on_floor():
+		current_state = State.Run
+		
+func player_jump(delta: float):
+	if Input.is_action_just_pressed("jump"):
+		current_state = State.Jump
+		velocity.y = jump
+	
+	if is_on_floor() and current_state == State.Jump:
+		direction = input_movement()
+		velocity.x += direction * jump_horizontal * delta
+		
+func player_animation():
+	if velocity.x > 0:
+		animation.flip_h = false
+	elif velocity.x < 0:
+		animation.flip_h = true
+	if current_state == State.Idle:
+		animation.play("idle")
+	elif current_state == State.Jump:
+		animation.play("jump")
+	elif current_state == State.Run:
+
+		animation.play("run")
+
+func input_movement():
+	var direction = Input.get_axis("left","right")
+	
+	return direction
